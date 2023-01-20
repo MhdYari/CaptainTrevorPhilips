38b69d42bad46b48c181baa9e468c8b8d6a2c54f
Mahdie Mohammadyari <93401343+MhdYari@users.noreply.github.com>
2023-01-20 05:50:45 +0330

    Clamping Function
    Clamping function on player plane

--- Captain-Trevor-Philips/Assets/Scripts/PlayerMovement.cs
+++ Captain-Trevor-Philips/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +16,21 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         float deltaX = Input.GetAxis("Horizontal")*Time.deltaTime*speed;
-        float deltaY = Input.GetAxis("Vertical")*Time.deltaTime*speed;;
+        float deltaY = Input.GetAxis("Vertical")*Time.deltaTime*speed;
 
         float newXpos = transform.position.x + deltaX;
         float newYpos = transform.position.y + deltaY;
 
         transform.position = new Vector2(newXpos, newYpos);
+
+        Vector3 posX = transform.position;
+        posX.x = Mathf.Clamp(posX.x,-2.5f,2.5f);
+        transform.position = posX;
+
+        Vector3 posY = transform.position;
+        posY.y = Mathf.Clamp(posY.y,-5.5f,5.5f);
+        transform.position = posY;
     }
+
+    
 }
